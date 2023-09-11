98cab2b3035f14f63521bb17e88a3527b1beb6c1
lucabasa <luca.basanisi@gmail.com>
2023-09-11 21:41:31 +0200

    added docstrings

--- datacyclist/powercurve.py
+++ datacyclist/powercurve.py
@@ -21,6 +21,25 @@ class PowerDict(dict):
         
         
 class PowerCurve(object):
+    """
+    This class computes the power curve of an activity and keeps track of the best result for each
+    time interval. The method that does the calculation is `calculate_curve` and it is supposed to run over
+    the data of 1 activity. It can be run over the data of several activities and to find the best power output
+    for a given time interval
+    
+    It finds the window with the best power output for a given amount of time (from 1 second to 5 hours)
+    It also records the corresponding average heart rate and cadence for that time interval (if available)
+    It uses the 20 minutes and/or the one hour average power to determine the FTP of the activity and the corresponding
+    heart rate (method in development)
+    
+    :Attributes:
+    sec_10: for example, for best results over 10 seconds. This attribute is a dictionary with
+    keys `activity best`, `activity_HR`, and `activity_cadence` for the best results of the activity in terms of power 
+    and corresponding heart rate and cadence averages (again, over 10 seconds in this example). Other keys are
+    `alltime_best` for the best result of all times in terms of 10 seconds power, and `alltime_HR` and `alltime_cadence`
+    for their corresponding heart rate and cadence values. These get updated every time the method is rerun over new data.
+    
+    """
     def __init__(self):
         self.sec_list = [1,2,5,10,20,30,
                          60,120,300,600,1200,
@@ -57,6 +76,10 @@ class PowerCurve(object):
             raise KeyError(f'These columns are missing in the data: {list(diff)}')
     
     def calculate_curve(self, activity_data):
+        """
+        This method finds the best power output over all the time intervals of the curve
+        It updates the class attributes.
+        """
         self._check_input(activity_data)
         self.data = activity_data.copy().set_index('timestamp')
         self.data = self.data.resample('1S').ffill()
@@ -71,6 +94,9 @@ class PowerCurve(object):
             self._rolling_avgs()            
 
     def get_activity_curve(self):
+        """
+        This method returns a dataframe with the values of the power curve, both for the activity and of all time
+        """
         activity_curve = []
         best_curve = []
         hr_curve = []
@@ -97,6 +123,12 @@ class PowerCurve(object):
     
     
     def get_ftp(self, data):
+        """
+        This method adds columns to the input data according to the power curve attributes
+        It finds the activity FTP and it updates the FTP of the rider if the value improved
+        """
+        # todo: this method only looks for improvements in FTP, we can use the threshold HR to find decreases in FTP
+        # as in: same threshold HR, lower 20 minutes power
         for sec in [1200, 3600]:
             attr = getattr(self, f'sec_{sec}')
             data[f'{int(sec / 60)}min_activity_power'] = np.where(attr['activity_best'] == 0, np.nan, attr['activity_best'])
